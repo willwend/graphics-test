b62e5ba9748ff1b97ff06a75fdd6e3b5a4aead50
William Wendorf <williamwendorf@Williams-MacBook-Pro.local>
2018-12-21 23:57:38 -0500

    Triangle generated

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 all:
-	g++ test.cpp -o test -I include -L lib -l SDL2-2.0.0 -l GLEW.2.1.0
+	g++ test.cpp --std=c++11 -o test -I include -L lib -l SDL2-2.0.0 -l GLEW.2.1.0 -framework OpenGL -Wno-deprecated
--- test.cpp
+++ test.cpp
@@ -3,6 +3,24 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_opengl.h>
 
+const GLchar* vertexSource = R"glsl(
+    #version 150 core
+    in vec2 position;
+    void main()
+    {
+        gl_Position = vec4(position, 0.0, 1.0);
+    }
+)glsl";
+
+const GLchar* fragmentSource = R"glsl(
+    #version 150 core
+    out vec4 outColor;
+    void main()
+    {
+        outColor = vec4(1.0, 1.0, 1.0, 1.0);
+    }
+)glsl";
+
 int main(int argc, char *argv[]) {
     SDL_Init(SDL_INIT_VIDEO);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
@@ -13,19 +31,81 @@ int main(int argc, char *argv[]) {
     SDL_GLContext context = SDL_GL_CreateContext(window);
     glewExperimental = GL_TRUE;
     glewInit();
-    GLuint vertexBuffer;
-    glGenBuffers(1, &vertexBuffer);
-    printf("%u\n", vertexBuffer);
     SDL_Event windowEvent;
-    while (true)
-    {
-        if (SDL_PollEvent(&windowEvent))
-        {
+
+    float vertices[] = {
+     0.5f,  0.5f, // Vertex 1 (X, Y)
+     0.5f, -0.5f, // Vertex 2 (X, Y)
+    -0.5f, -0.5f  // Vertex 3 (X, Y)
+    };
+
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    GLuint vbo;
+    glGenBuffers(1, &vbo); // Generate 1 buffer
+    printf("Buffer status: %u\n", vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexSource, NULL);
+    glCompileShader(vertexShader);
+    GLint status;
+
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &status);
+    if (status == GL_TRUE) {
+        printf("Vertex shader compiled successfully.\n");
+    }
+    else {
+        char buffer[512];
+        glGetShaderInfoLog(vertexShader, 512, NULL, buffer);
+        printf("Vertex shader failed to compile:\n%s", buffer);
+    }
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+    glCompileShader(fragmentShader);
+
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &status);
+    if (status == GL_TRUE) {
+        printf("Fragment shader compiled successfully.\n");
+    }
+    else {
+        char buffer[512];
+        glGetShaderInfoLog(fragmentShader, 512, NULL, buffer);
+        printf("Fragment shader failed to compile:\n%s", buffer);
+    }
+
+    GLuint shaderProgram = glCreateProgram(); // combine into program
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glBindFragDataLocation(shaderProgram, 0, "outColor"); // which buffer - "glDrawBuffers"
+    glLinkProgram(shaderProgram);
+    glUseProgram(shaderProgram);
+
+    GLint posAttrib = glGetAttribLocation(shaderProgram, "position"); // ref to input
+    glEnableVertexAttribArray(posAttrib);
+    glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 0, 0); // how to retrieve
+
+    while (true) {
+        if (SDL_PollEvent(&windowEvent)) {
             if (windowEvent.type == SDL_QUIT) break;
             if (windowEvent.type == SDL_KEYUP && windowEvent.key.keysym.sym == SDLK_ESCAPE) break;
         }
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Clear the screen to black
+        glClear(GL_COLOR_BUFFER_BIT);
+        glDrawArrays(GL_TRIANGLES, 0, 3); // Draw a triangle from the 3 vertices
         SDL_GL_SwapWindow(window);
     }
+
+    glDeleteProgram(shaderProgram);
+    glDeleteShader(fragmentShader);
+    glDeleteShader(vertexShader);
+    glDeleteBuffers(1, &vbo);
+    glDeleteVertexArrays(1, &vao);
+
     SDL_GL_DeleteContext(context);
     SDL_Quit();
     return 0;
