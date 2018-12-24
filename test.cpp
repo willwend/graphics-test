cc1f775627fda75a87e59373a5ec3544eadb3795
William Wendorf <williamwendorf@Williams-MacBook-Pro.local>
2018-12-24 09:36:07 -0500

    Stencils done.

--- test.cpp
+++ test.cpp
@@ -11,25 +11,26 @@
 const GLchar* vertexSource = R"glsl(
     #version 150 core
 
-    in vec2 position;
+    in vec3 position;
     in vec3 color;
-    in vec2 texcoordone;
+    //in vec2 texcoordone;
     in vec2 texcoordtwo;
 
     out vec3 Color;
-    out vec2 Texcoordone;
+    //out vec2 Texcoordone;
     out vec2 Texcoordtwo;
 
     uniform mat4 model;
     uniform mat4 view;
     uniform mat4 proj;
+    uniform vec3 overrideColor;
 
     void main()
     {
-        Color = color;
-        Texcoordone = texcoordone;
+        Color = overrideColor * color;
+        //Texcoordone = texcoordone;
         Texcoordtwo = texcoordtwo;
-        gl_Position = proj * view * model * vec4(position, 0.0, 1.0);
+        gl_Position = proj * view * model * vec4(position, 1.0);
     }
 )glsl";
 
@@ -47,7 +48,8 @@ const GLchar* fragmentSource = R"glsl(
     
     void main()
     {
-        outColor = mix(texture(texOne, Texcoordone), texture(texTwo, Texcoordtwo), 0.5) * vec4(Color, 1.0);
+        //outColor = mix(texture(texOne, Texcoordone), texture(texTwo, Texcoordtwo), 0.5) * vec4(Color, 1.0);
+        outColor = texture(texTwo, Texcoordtwo) * vec4(Color, 1.0);
     }
 )glsl";
 
@@ -64,17 +66,61 @@ int main(int argc, char *argv[]) {
     SDL_Event windowEvent;
 
     GLfloat vertices[] = {
-    //  Position      Color             Texcoords
-        -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 0.25f, 0.45f, 0.0f, 0.0f, // Top-left
-         0.5f,  0.5f, 0.7f, 0.7f, 0.7f, 0.55f, 0.25f, 1.0f, 0.0f, // Top-right
-         0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.75f, 0.55f, 1.0f, 1.0f, // Bottom-right
-        -0.5f, -0.5f, 0.5f, 0.5f, 0.5f, 0.45f, 0.75f, 0.0f, 1.0f  // Bottom-left
+    //  Position            Color             Texcoords
+        -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,
+         0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
+         0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,
+
+        -0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,
+         0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,
+
+        -0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
+
+         0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
+         0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,
+         0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,
+         0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
+
+        -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
+         0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
+         0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,
+
+        -0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,
+         0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,
+
+        -1.0f, -1.0f, -0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, // floor
+         1.0f, -1.0f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f,
+         1.0f,  1.0f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f,
+         1.0f,  1.0f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f,
+        -1.0f,  1.0f, -0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f,
+        -1.0f, -1.0f, -0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f
     };
 
-    GLuint elements[] = {
+    /*GLuint elements[] = {
         0, 1, 2,
         2, 3, 0
-    };
+    };*/
 
     GLuint vao;
     glGenVertexArrays(1, &vao);
@@ -85,10 +131,10 @@ int main(int argc, char *argv[]) {
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    GLuint ebo;
+    /*GLuint ebo;
     glGenBuffers(1, &ebo); // Create an element array
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(elements), elements, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(elements), elements, GL_STATIC_DRAW);*/
 
     GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &vertexSource, NULL);
@@ -129,25 +175,25 @@ int main(int argc, char *argv[]) {
     // Specify the layout of the vertex data
     GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
     glEnableVertexAttribArray(posAttrib);
-    glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 9 * sizeof(GLfloat), 0);
+    glVertexAttribPointer(posAttrib, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), 0);
 
     GLint colAttrib = glGetAttribLocation(shaderProgram, "color");
     glEnableVertexAttribArray(colAttrib);
-    glVertexAttribPointer(colAttrib, 3, GL_FLOAT, GL_FALSE, 9 * sizeof(GLfloat), (void*)(2 * sizeof(GLfloat)));
+    glVertexAttribPointer(colAttrib, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat)));
 
-    GLint texoneAttrib = glGetAttribLocation(shaderProgram, "texcoordone");
+    /*GLint texoneAttrib = glGetAttribLocation(shaderProgram, "texcoordone");
     glEnableVertexAttribArray(texoneAttrib);
-    glVertexAttribPointer(texoneAttrib, 2, GL_FLOAT, GL_FALSE, 9 * sizeof(GLfloat), (void*)(7 * sizeof(GLfloat)));
+    glVertexAttribPointer(texoneAttrib, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (void*)(6 * sizeof(GLfloat)));*/
 
     GLint textwoAttrib = glGetAttribLocation(shaderProgram, "texcoordtwo");
     glEnableVertexAttribArray(textwoAttrib);
-    glVertexAttribPointer(textwoAttrib, 2, GL_FLOAT, GL_FALSE, 9 * sizeof(GLfloat), (void*)(5 * sizeof(GLfloat)));
+    glVertexAttribPointer(textwoAttrib, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (void*)(6 * sizeof(GLfloat)));
 
     // Load texture
     GLuint textures[2];
     glGenTextures(2, textures);
 
-    glActiveTexture(GL_TEXTURE0);
+    /*glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, textures[0]);
     int width, height;
     unsigned char* image = SOIL_load_image("SOIL/img_test.png", &width, &height, 0, SOIL_LOAD_RGB);
@@ -157,7 +203,7 @@ int main(int argc, char *argv[]) {
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);*/
 
     float pixels[] = {
         0.8f, 0.0f, 0.0f,   0.9f, 0.9f, 0.0f,
@@ -167,31 +213,35 @@ int main(int argc, char *argv[]) {
     glBindTexture(GL_TEXTURE_2D, textures[1]);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 2, 2, 0, GL_RGB, GL_FLOAT, pixels);
     glUniform1i(glGetUniformLocation(shaderProgram, "texTwo"), 1);
+
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
-    auto t_start = std::chrono::high_resolution_clock::now();
-    glm::mat4 model = glm::mat4(1.0f);
-    model = glm::rotate(model, glm::radians(180.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+    /*glm::mat4 model = glm::mat4(1.0f);
+    model = glm::rotate(model, glm::radians(180.0f), glm::vec3(0.0f, 0.0f, 1.0f));*/
     GLint uniModel = glGetUniformLocation(shaderProgram, "model");
-    glUniformMatrix4fv(uniModel, 1, GL_FALSE, glm::value_ptr(model));
+    //glUniformMatrix4fv(uniModel, 1, GL_FALSE, glm::value_ptr(model));
 
     glm::mat4 view = glm::lookAt(
-    glm::vec3(2.0f, 3.0f, 5.0f),
-    glm::vec3(0.0f, 0.0f, 0.0f),
-    glm::vec3(0.0f, 0.0f, 1.0f)
+        glm::vec3(2.0f, 3.0f, 5.0f),
+        glm::vec3(0.0f, 0.0f, 0.0f),
+        glm::vec3(0.0f, 0.0f, 1.0f)
     );
     GLint uniView = glGetUniformLocation(shaderProgram, "view");
     glUniformMatrix4fv(uniView, 1, GL_FALSE, glm::value_ptr(view));
+
     glm::mat4 proj = glm::perspective(glm::radians(40.0f), 800.0f / 600.0f, 1.0f, 30.0f);
     GLint uniProj = glGetUniformLocation(shaderProgram, "proj");
     glUniformMatrix4fv(uniProj, 1, GL_FALSE, glm::value_ptr(proj));
+
     float sint = 1.6f;
     bool flip = false;
-    //GLfloat angle = -45.0f;
-    //GLfloat speed = 0.0f;
+    auto t_start = std::chrono::high_resolution_clock::now();
+    glEnable(GL_DEPTH_TEST);
+    GLint uniColor = glGetUniformLocation(shaderProgram, "overrideColor");
+    glm::mat4 model, old = glm::mat4(1.0f);
 
     while (true) {
         if (SDL_PollEvent(&windowEvent)) {
@@ -201,28 +251,64 @@ int main(int argc, char *argv[]) {
                 if (windowEvent.key.keysym.sym == SDLK_SPACE) flip = !flip;
             }
         }
+
+        glClearColor(0.5f, 0.5f, 0.5f, 1.0f); // Clear the screen to black
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glUniform3f(uniColor, 1.0f, 1.0f, 1.0f); // Start with no darkening
+
         auto t_now = std::chrono::high_resolution_clock::now();
         float time = std::chrono::duration_cast<std::chrono::duration<float>>(t_now - t_start).count();
-        if (time > 0.015f) {
-            sint += time;
-            t_start = t_now;
-            float scaling = sin(sint) * 0.01f + 1.0f;
-            if (flip) {
-                model = glm::rotate(
-                    model,
-                    glm::radians(1.0f),
-                    glm::vec3(1.0f, 0.0f, 0.0f)
-                );
-            }
-            model = glm::scale(
+        sint += time;
+        t_start = t_now;
+        float scaling = sin(sint) * time + 1.0f;
+
+        model = old;
+        if (flip) {
+            model = glm::rotate(
                 model,
-                glm::vec3(scaling, scaling, scaling)
+                glm::radians(time * 40.0f),
+                glm::vec3(1.0f, 0.0f, 0.0f)
             );
-            glUniformMatrix4fv(uniModel, 1, GL_FALSE, glm::value_ptr(model));
         }
-        glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Clear the screen to black
-        glClear(GL_COLOR_BUFFER_BIT);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0); // Draw rectangle
+        model = glm::scale(
+            model,
+            glm::vec3(scaling, scaling, scaling)
+        );
+        glUniformMatrix4fv(uniModel, 1, GL_FALSE, glm::value_ptr(model));
+
+        glDrawArrays(GL_TRIANGLES, 0, 36); // Draw cube
+        old = model;
+        glEnable(GL_STENCIL_TEST); // STENCIL START
+        
+        // Draw floor
+        glStencilFunc(GL_ALWAYS, 1, 0xFF);
+        glStencilOp(GL_ZERO, GL_ZERO, GL_REPLACE);
+        glStencilMask(0xFF);
+        glDepthMask(GL_FALSE);
+        //glColorMask(GL_FALSE, GL_FALSE, GL_FALSE, GL_FALSE);
+        glClear(GL_STENCIL_BUFFER_BIT);
+
+        glDrawArrays(GL_TRIANGLES, 36, 6);
+
+        // Draw cube reflection
+        glStencilFunc(GL_EQUAL, 1, 0xFF);
+        glStencilMask(0x00);
+        glDepthMask(GL_TRUE);
+        //glColorMask(GL_TRUE, GL_TRUE, GL_TRUE, GL_TRUE);
+
+        model = glm::scale(
+            glm::translate(model, glm::vec3(0, 0, -1)),
+            glm::vec3(1, 1, -1)
+        );
+        glUniformMatrix4fv(uniModel, 1, GL_FALSE, glm::value_ptr(model));
+
+        glUniform3f(uniColor, 0.3f, 0.3f, 0.3f); // Darken override
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        glDisable(GL_STENCIL_TEST); // STENCIL END
+
+        //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0); // Draw rectangle
+
         SDL_GL_SwapWindow(window);
     }
 
