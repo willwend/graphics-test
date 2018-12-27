3ee44d4256ed20175b06919a825a945b3b51328f
William Wendorf <williamwendorf@Williams-MacBook-Pro.local>
2018-12-26 20:49:26 -0500

    Added sun (two programs with different shaders)

--- test.cpp
+++ test.cpp
@@ -28,16 +28,43 @@ const GLchar* vertexSource = R"glsl(
     }
 )glsl";
 
+const GLchar* sunVSource = R"glsl(
+    #version 150 core
+    in vec3 position;
+
+    uniform mat4 model;
+    uniform mat4 view;
+    uniform mat4 proj;
+
+    void main()
+    {
+        gl_Position = proj * view * model * vec4(position, 1.0);
+    }
+)glsl";
+
 const GLchar* fragmentSource = R"glsl(
     #version 150 core
     in vec2 Texcoord;
     out vec4 outColor;
 
     uniform sampler2D currTexture;
+    uniform vec3 lightColor;
 
     void main()
     {
-        outColor = texture(currTexture, Texcoord);
+        outColor = texture(currTexture, Texcoord) * vec4(lightColor, 1.0);
+    }
+)glsl";
+
+const GLchar* sunFSource = R"glsl(
+    #version 150 core
+    out vec4 FragColor;
+
+    uniform vec3 lightColor;
+    
+    void main()
+    {
+        FragColor = vec4(lightColor, 1.0); // set all 4 vector values to 1.0
     }
 )glsl";
 
@@ -74,7 +101,6 @@ void makeTexture(const char* filename, const char* shadername, GLenum id, size_t
 }
 
 void addVertices(float d, float u, float v, std::vector<float> &vertices, float pi) {
-    printf("pushing:%f,%f,%f,%f,%f\n", d * cos(u)*sin(v), d * sin(u)*sin(v), d * cos(v), u / (2 * pi), v / pi);
     vertices.push_back(d * cos(u)*sin(v)); // x
     vertices.push_back(d * sin(u)*sin(v)); // y
     vertices.push_back(d * cos(v)); // z
@@ -88,9 +114,7 @@ std::vector<float> makeGlobe(float d, int res) {
     float safe = 0.00001;
     vertices.reserve(6 * 5 * 2 * res * res);
     for (float u = 0.0; u < (2 * pi) - safe; u += pi / res) {
-        printf("u = %f\n", u);
         for (float v = 0.0; v < pi - safe; v += pi / res) {
-            printf("v = %f\n", v);
             addVertices(d, u, v, vertices, pi);
             addVertices(d, u + pi / res, v, vertices, pi);
             addVertices(d, u, v + pi / res, vertices, pi);
@@ -120,30 +144,44 @@ int main(int argc, char *argv[]) {
     GLuint moona;
     glGenVertexArrays(1, &moona);
     glBindVertexArray(moona);
+    GLuint suna;
+    glGenVertexArrays(1, &suna);
+    glBindVertexArray(suna);
 
     GLuint earthb;
     glGenBuffers(1, &earthb);
-    std::vector<float> earthbuf = makeGlobe(4, 12);
+    std::vector<float> earthbuf = makeGlobe(0.5, 12);
     GLuint moonb;
     glGenBuffers(1, &moonb);
-    std::vector<float> moonbuf = makeGlobe(1, 12);
+    std::vector<float> moonbuf = makeGlobe(0.25, 24);
+    GLuint sunb;
+    glGenBuffers(1, &sunb);
+    std::vector<float> sunbuf = makeGlobe(6.0, 12);
 
     glBindBuffer(GL_ARRAY_BUFFER, earthb);
     glBufferData(GL_ARRAY_BUFFER, earthbuf.size() * 4, &earthbuf[0], GL_STATIC_DRAW);
     glBindBuffer(GL_ARRAY_BUFFER, moonb);
     glBufferData(GL_ARRAY_BUFFER, moonbuf.size() * 4, &moonbuf[0], GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, sunb);
+    glBufferData(GL_ARRAY_BUFFER, sunbuf.size() * 4, &sunbuf[0], GL_STATIC_DRAW);
 
     GLuint vertexShader = makeShader(GL_VERTEX_SHADER, vertexSource);
     GLuint fragmentShader = makeShader(GL_FRAGMENT_SHADER, fragmentSource);
+    GLuint sunVShader = makeShader(GL_VERTEX_SHADER, sunVSource);
+    GLuint sunFShader = makeShader(GL_FRAGMENT_SHADER, sunFSource);
 
     GLuint shaderProgram = glCreateProgram();
     glAttachShader(shaderProgram, vertexShader);
     glAttachShader(shaderProgram, fragmentShader);
-    glBindFragDataLocation(shaderProgram, 0, "outColor"); // which buffer - "glDrawBuffers"
     glLinkProgram(shaderProgram);
     glUseProgram(shaderProgram);
 
-    glBindVertexArray(eartha);
+    GLuint sunProgram = glCreateProgram();
+    glAttachShader(sunProgram, sunVShader);
+    glAttachShader(sunProgram, sunFShader);
+    glLinkProgram(sunProgram);
+
+    glBindVertexArray(eartha); // to do: merge these three into one
     glBindBuffer(GL_ARRAY_BUFFER, earthb);
     glEnableVertexAttribArray(0);
     GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
@@ -161,30 +199,41 @@ int main(int argc, char *argv[]) {
     texAttrib = glGetAttribLocation(shaderProgram, "texcoord");
     glVertexAttribPointer(texAttrib, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (char*)(3 * sizeof(float)));
 
+    glUseProgram(sunProgram);
+    glBindVertexArray(suna);
+    glBindBuffer(GL_ARRAY_BUFFER, sunb);
+    glEnableVertexAttribArray(0);
+    posAttrib = glGetAttribLocation(sunProgram, "position");
+    glVertexAttribPointer(posAttrib, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), 0);
+
+    glUseProgram(shaderProgram);
     GLuint textures[2];
     glGenTextures(2, textures);
     makeTexture("earth.jpg", "texEarth", GL_TEXTURE0, 0, textures, shaderProgram);
     makeTexture("moon.jpg", "texMoon", GL_TEXTURE1, 1, textures, shaderProgram);
 
-    GLint uniModel = glGetUniformLocation(shaderProgram, "model");
+    glm::vec3 lightColor(0.95f, 1.0f, 0.81f);
+    glm::vec3 lightPos(20.0f, 800.0f, 1.0f);
+
     glm::mat4 earthmodel = glm::mat4(1.0f);
     glm::mat4 moonmodel = glm::mat4(1.0f);
+    glm::mat4 sunmodel = glm::mat4(1.0f);
     moonmodel = glm::translate(
         moonmodel,
         glm::vec3(0.0f, 36.0f, 0.0f)
     );
+    sunmodel = glm::translate(
+        moonmodel,
+        lightPos
+    );
 
     glm::mat4 view = glm::lookAt(
-        glm::vec3(-0.2f, 38.5f, 1.0f),
-        glm::vec3(0.0f, 0.0f, 0.0f),
+        glm::vec3(-1.0f, -36.8f, 1.0f),
+        glm::vec3(0.0f, 0.0f, 1.0f),
         glm::vec3(0.0f, 0.0f, 1.0f)
     );
-    GLint uniView = glGetUniformLocation(shaderProgram, "view");
-    glUniformMatrix4fv(uniView, 1, GL_FALSE, glm::value_ptr(view));
 
-    glm::mat4 proj = glm::perspective(glm::radians(60.0f), 800.0f / 600.0f, 1.0f, 70.0f);
-    GLint uniProj = glGetUniformLocation(shaderProgram, "proj");
-    glUniformMatrix4fv(uniProj, 1, GL_FALSE, glm::value_ptr(proj));
+    glm::mat4 proj = glm::perspective(glm::radians(5.0f), 800.0f / 600.0f, 0.1f, 1000.0f);
 
     bool rotate = false;
     auto t_start = std::chrono::high_resolution_clock::now();
@@ -205,6 +254,14 @@ int main(int argc, char *argv[]) {
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+        glUseProgram(shaderProgram);
+        GLint uniView = glGetUniformLocation(shaderProgram, "view");
+        glUniformMatrix4fv(uniView, 1, GL_FALSE, glm::value_ptr(view));
+        GLint uniProj = glGetUniformLocation(shaderProgram, "proj");
+        glUniformMatrix4fv(uniProj, 1, GL_FALSE, glm::value_ptr(proj));
+        GLint uniLight = glGetUniformLocation(shaderProgram, "lightColor");
+        glUniform3fv(uniLight, 1, glm::value_ptr(lightColor));
+
         if (rotate && time > 0.02f) {
             t_start = t_now;
             earthmodel = glm::rotate(
@@ -222,14 +279,28 @@ int main(int argc, char *argv[]) {
         glActiveTexture(GL_TEXTURE0);
         glUniform1i(glGetUniformLocation(shaderProgram, "currTexture"), 0);
         glBindVertexArray(eartha);
+        GLint uniModel = glGetUniformLocation(shaderProgram, "model");
         glUniformMatrix4fv(uniModel, 1, GL_FALSE, glm::value_ptr(earthmodel));
         glDrawArrays(GL_TRIANGLES, 0, earthbuf.size());
 
         glActiveTexture(GL_TEXTURE1);
         glUniform1i(glGetUniformLocation(shaderProgram, "currTexture"), 1);
         glBindVertexArray(moona);
+        uniModel = glGetUniformLocation(shaderProgram, "model");
         glUniformMatrix4fv(uniModel, 1, GL_FALSE, glm::value_ptr(moonmodel));
         glDrawArrays(GL_TRIANGLES, 0, moonbuf.size());
+
+        glUseProgram(sunProgram);
+        glBindVertexArray(suna);
+        GLint sunModel = glGetUniformLocation(sunProgram, "model");
+        glUniformMatrix4fv(sunModel, 1, GL_FALSE, glm::value_ptr(sunmodel));
+        GLint sunView = glGetUniformLocation(sunProgram, "view");
+        glUniformMatrix4fv(sunView, 1, GL_FALSE, glm::value_ptr(view));
+        GLint sunProj = glGetUniformLocation(sunProgram, "proj");
+        glUniformMatrix4fv(sunProj, 1, GL_FALSE, glm::value_ptr(proj));
+        GLint sunLight = glGetUniformLocation(sunProgram, "lightColor");
+        glUniform3fv(sunLight, 1, glm::value_ptr(lightColor));
+        glDrawArrays(GL_TRIANGLES, 0, sunbuf.size());
 
         SDL_GL_SwapWindow(window);
     }
@@ -237,8 +308,10 @@ int main(int argc, char *argv[]) {
     glDeleteProgram(shaderProgram);
     glDeleteShader(fragmentShader);
     glDeleteShader(vertexShader);
+    glDeleteBuffers(1, &sunb);
     glDeleteBuffers(1, &moonb);
     glDeleteBuffers(1, &earthb);
+    glDeleteVertexArrays(1, &suna);
     glDeleteVertexArrays(1, &moona);
     glDeleteVertexArrays(1, &eartha);
 
